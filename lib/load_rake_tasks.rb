1a57a9113d4e1f4e222f353791bdf0cdf5d634fc
Oscar Ortega <ortegaoscar14@gmail.com>
2024-12-02 21:18:54 -0500

    :construction: remove task

--- lib/load_rake_tasks.rb
+++
@@ -1,9 +0,0 @@
-# lib/load_rake_tasks.rb
-require 'rake'
-
-# Inicializa el entorno de Rake
-Rake.application.init
-Rake.application.load_rakefile
-
-# Ejecuta la tarea
-Rake.application['luminous_rails_ui:example'].invoke
